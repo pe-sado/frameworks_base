4ecdc72a52a1a9e2a4a838a2c981b3b1de130cff
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-07-29 17:12:24 +0800

    [SQUASHED] base: Add status bar lyric for Android 14
    * This is a squash of:
    
    base: Add status bar lyric [1/3]
    
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: I5c49187b0752286ee00a60d961203c0c4ec0d555
    
    fixup! base: Add status bar lyric [1/3]
    
     * java.lang.SecurityException: Settings key: <status_bar_show_lyric> is
       not readable. From S+, settings keys annotated with @hide are
       restricted to system_server and system apps only, unless they are
       annotated with @Readable.
    
    Change-Id: Ia0c209f3ddd5bbe1be676e172f408b3faf8cfe81
    
    base: Adapt StatusBar Lyric for Miku UI Udon
    
    Signed-off-by: AmeChanRain <downloadbot007@gmail.com>
    
    Co-authored-by: LynnrinChan <wuxiaofei31@gmail.com>
    Co-authored-by: AmeChanRain <downloadbot007@gmail.com>
    Change-Id: I55d8299268a633de9b8b9fcb1d9ebab7dbe901fc

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
@@ -104,6 +104,8 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
     private boolean mAnimationsEnabled = true;
     private final KeyguardStateController mKeyguardStateController;
 
+    private LyricViewController mLyricViewController;
+
     @VisibleForTesting
     @Inject
     public HeadsUpAppearanceController(
@@ -235,6 +237,10 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
         }
     }
 
+    public void setLyricViewController(LyricViewController controller) {
+        mLyricViewController = controller;
+    }
+
     private void setShown(boolean isShown) {
         if (mShown != isShown) {
             mShown = isShown;
@@ -244,12 +250,18 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
                 show(mView);
                 hide(mClockView, View.INVISIBLE);
                 mOperatorNameViewOptional.ifPresent(view -> hide(view, View.INVISIBLE));
+                if (mLyricViewController != null) {
+                    mLyricViewController.hideLyricView(mAnimationsEnabled);
+                }
             } else {
                 show(mClockView);
                 mOperatorNameViewOptional.ifPresent(this::show);
                 hide(mView, View.GONE, () -> {
                     updateParentClipping(true /* shouldClip */);
                 });
+                if (mLyricViewController != null) {
+                    mLyricViewController.showLyricView(mAnimationsEnabled);
+                }
             }
             // Show the status bar icons when the view gets shown / hidden
             if (mStatusBarStateController.getState() != StatusBarState.SHADE) {
